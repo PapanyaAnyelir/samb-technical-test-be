7743d9f0b99660e061617436d16083ef1263d8cf
AnotherRizz <risqijapana55368@gmail.com>
2024-11-30 20:06:10 +0700

    Add .env file

--- models/setup.go
+++ models/setup.go
@@ -1,49 +1,65 @@
 package models
 
 import (
+    "fmt"
+    "log"
+    "os"
 
-	"log"
-
-
-	"gorm.io/driver/mysql"
-	"gorm.io/gorm"
+    "github.com/joho/godotenv"
+    "gorm.io/driver/mysql"
+    "gorm.io/gorm"
 )
 
 var DB *gorm.DB
 
 func ConnectDatabase() {
-database, err := gorm.Open(mysql.Open("root:@tcp(localhost:3306)/samb_db_warehouse?charset=utf8mb4&parseTime=True&loc=Local"), &gorm.Config{})
-	if err != nil {
-		panic(err)
-	}
+    err := godotenv.Load()
+    if err != nil {
+        log.Fatalf("Error loading .env file: %v", err)
+    }
 
-	database.AutoMigrate(&Product{})
-	database.AutoMigrate(&Supplier{})
-	database.AutoMigrate(&Customer{})
-	database.AutoMigrate(&Warehouse{})
-	database.AutoMigrate(&PenerimaanBarangHeader{})
-	database.AutoMigrate(&PenerimaanBarangDetail{})
-	database.AutoMigrate(&PengeluaranBarangHeader{})
-	database.AutoMigrate(&PengeluaranBarangDetail{})
 
-	DB = database
+    dbUser := os.Getenv("DB_USER")
+    dbPass := os.Getenv("DB_PASS")
+    dbHost := os.Getenv("DB_HOST")
+    dbPort := os.Getenv("DB_PORT")
+    dbName := os.Getenv("DB_NAME")
+    dbCharset := os.Getenv("DB_CHARSET")
+    dbParseTime := os.Getenv("DB_PARSE_TIME")
+    dbLoc := os.Getenv("DB_LOC")
 
-	log.Println("Menjalankan seeder...")
-	if err := SeedProducts(); err != nil {
-		log.Printf("Gagal menjalankan seeder Product: %v", err)
-	}
 
-	if err := SeedCustomers(); err != nil {
-		log.Printf("Gagal menjalankan seeder Customer: %v", err)
-	}
+    dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=%s&parseTime=%s&loc=%s",
+        dbUser, dbPass, dbHost, dbPort, dbName, dbCharset, dbParseTime, dbLoc)
 
-	if err := SeedWarehouses(); err != nil {
-		log.Printf("Gagal menjalankan seeder Warehouse: %v", err)
-	}
 
-	if err := SeedSuppliers(); err != nil {
-		log.Printf("Gagal menjalankan seeder Supplier: %v", err)
-	}
+    database, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+    if err != nil {
+        panic(err)
+    }
 
-	log.Println("Semua seeder berhasil dijalankan")
+    database.AutoMigrate(&Product{}, &Supplier{}, &Customer{}, &Warehouse{},
+        &PenerimaanBarangHeader{}, &PenerimaanBarangDetail{},
+        &PengeluaranBarangHeader{}, &PengeluaranBarangDetail{})
+
+    DB = database
+
+    log.Println("Menjalankan seeder...")
+    if err := SeedProducts(); err != nil {
+        log.Printf("Gagal menjalankan seeder Product: %v", err)
+    }
+
+    if err := SeedCustomers(); err != nil {
+        log.Printf("Gagal menjalankan seeder Customer: %v", err)
+    }
+
+    if err := SeedWarehouses(); err != nil {
+        log.Printf("Gagal menjalankan seeder Warehouse: %v", err)
+    }
+
+    if err := SeedSuppliers(); err != nil {
+        log.Printf("Gagal menjalankan seeder Supplier: %v", err)
+    }
+
+    log.Println("Semua seeder berhasil dijalankan")
 }
